ff782324a7e839a011d0cfd7269bd9341412beda
Nahidjc <nahidjc80@gmail.com>
2021-11-09 18:27:55 +0600

    data show in ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 
 import React, { Component } from "react";
 import CustomModal from "./Components/CustomModal/CustomModal";
+import Axios from "axios";
+import { headers } from "./Components/Shared/Shared";
 
 class App extends Component {
   constructor(props) {
@@ -28,6 +30,109 @@ class App extends Component {
     this.setState({ activeItem: item, modal: !this.state.modal });
   };
 
+  handleSubmit = (item) => {
+    this.toggle();
+    console.log(item);
+    if (item.id) {
+      // if old post to edit and submit
+      Axios({
+        url: `http://localhost:8000/api/tasks/${item.id}/`,
+        method: "PUT",
+        headers: headers,
+        data: item,
+      }).then((res) => console.log(res));
+      return;
+    }
+    // if new post to submit
+
+    Axios({
+      url: "http://127.0.0.1:8000/api/tasks/",
+      method: "POST",
+      headers: headers,
+      data: item,
+    }).then((res) => console.log(res));
+  };
+
+  componentDidMount() {
+    console.log("get data");
+    this.refreshList();
+  }
+
+  refreshList = () => {
+    Axios({
+      url: "http://127.0.0.1:8000/api/tasks/",
+      method: "GET",
+      headers: headers,
+    })
+      .then((res) => {
+        this.setState({ taskList: res.data });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  displayCompleted = (status) => {
+    if (status) {
+      return this.setState({ viewCompleted: true });
+    }
+    return this.setState({ viewCompleted: false });
+  };
+
+  renderTabList = () => {
+    return (
+      <div className="my-5 tab-list">
+        <span
+          onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "active" : ""}
+        >
+          completed
+        </span>
+        <span
+          onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "" : "active"}
+        >
+          Incompleted
+        </span>
+      </div>
+    );
+  };
+
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.taskList.filter(
+      (item) => item.completed === viewCompleted
+    );
+    console.log(newItems);
+    return newItems.map((item) => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`todo-title mr-2 ${
+            this.state.viewCompleted ? "completed-todo" : ""
+          }`}
+          title={item.description}
+        >
+          {item.title}
+        </span>
+        <span>
+          <button
+            onClick={() => this.editItem(item)}
+            className="btn btn-secondary mr-2"
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => this.handleDelete(item)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
+        </span>
+      </li>
+    ));
+  };
+
   render() {
     return (
       <div>
@@ -44,7 +149,9 @@ class App extends Component {
                   </button>
                 </div>
 
-                <ul className="list-group list-group-flush"></ul>
+                <ul className="list-group list-group-flush">
+                  {this.renderItems()}
+                </ul>
               </div>
             </div>
           </div>
