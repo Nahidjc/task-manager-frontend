fe2e10279268a52c81b92e8558edb95df5652937
Nahidjc <nahidjc80@gmail.com>
2021-11-09 12:50:02 +0600

    modal added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,65 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+import React, { Component } from "react";
+import CustomModal from "./Components/CustomModal/CustomModal";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      modal: false,
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false,
+      },
+      taskList: [],
+    };
+  }
+
+  toggle = () => {
+    //add this after modal creation
+    this.setState({ modal: !this.state.modal }); //add this after modal creation
+  };
+
+  createItem = () => {
+    const item = { title: "", description: "", completed: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
+  render() {
+    return (
+      <div>
+        <main>
+          <h1 className="text-black text-uppercase text-center my-4">
+            Task Manager
+          </h1>
+          <div className="row ">
+            <div className="col-md-6 col-sm-10 mx-auto p-0">
+              <div className="card p-3">
+                <div className="">
+                  <button className="btn btn-primary" onClick={this.createItem}>
+                    Add task
+                  </button>
+                </div>
+
+                <ul className="list-group list-group-flush"></ul>
+              </div>
+            </div>
+          </div>
+
+          {this.state.modal ? (
+            <CustomModal
+              activeItem={this.state.activeItem}
+              toggle={this.toggle}
+              onSave={this.handleSubmit}
+            ></CustomModal>
+          ) : null}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
